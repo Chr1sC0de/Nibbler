60a7f54cb5d9194717142dc047d48f95bc1d8d16
Chris <63153273+Chr1sC0de@users.noreply.github.com>
2020-05-13 17:17:44 +1000

    update

--- tests/api/binance/api_trader/trader.py
+++ tests/api/binance/api_trader/trader.py
@@ -2,6 +2,8 @@ from nibbler import trading as td
 import myconfig as mc
 from nibbler import api
 from pathlib import Path 
+from nibbler.trading.stops import atr_stopper
+import numpy as np
 
 if __name__ == "__main__":
     # setup the collector
@@ -15,10 +17,23 @@ if __name__ == "__main__":
     collector.run(filepath)
 
     # setup the bull, bear and stopper
+    signal_paths = Path(__file__).parent/'jljkolj'
 
+    if signal_paths.exists():
+        data = np.load(signal_paths)
+        bull_signal = data["bull_signal"]
+        bear_signal = data["bear_signal"]
+    else:
+        raise Exception
+
+    # setup the stop calculator
+    stop_calculator = atr_stopper
 
     # setup the trading boi
 
     agent = api.agents.binance.NibblerAllInAgent(
-
+        collector,
+        bull_signal,
+        bear_signal,
+        stop_calculator=stop_calculator
     )
